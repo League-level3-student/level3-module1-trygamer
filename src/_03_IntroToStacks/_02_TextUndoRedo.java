8d203cac9172310dfba200363b5d1beb0e08064c
William Kwak <League@AdministorsiMac.attlocal.net>
2019-02-02 17:29:32 -0800

    Bye

--- src/_03_IntroToStacks/_02_TextUndoRedo.java
+++ src/_03_IntroToStacks/_02_TextUndoRedo.java
@@ -1,10 +1,14 @@
 package _03_IntroToStacks;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Stack;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class _02_TextUndoRedo {
+public class _02_TextUndoRedo implements KeyListener {
 	/* 
 	 * Create a JFrame with a JPanel and a JLabel.
 	 * 
@@ -18,6 +22,14 @@ public class _02_TextUndoRedo {
 	 * 
 	 * */
 	
+	public static void main(String[] args) {
+		_02_TextUndoRedo tur = new _02_TextUndoRedo();
+		
+		tur.SetUp();
+	}
+	
+	Stack<Character> cr = new Stack<Character>();
+	Stack<Character> crd = new Stack<Character>();
 	JFrame f = new JFrame();
 	JPanel p = new JPanel();
 	JLabel l = new JLabel();
@@ -26,11 +38,54 @@ public class _02_TextUndoRedo {
 	f.setVisible(true);
 	f.add(p);
 	p.add(l);
-	f.addKeyListeners();
+	f.addKeyListener(this);
 	f.pack();
 	
 	
 	
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+	System.out.println(e.getKeyChar());
+	
+	
+	
+	
+	
+	
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		cr.push(e.getKeyChar());
+		for(int i= 0; i<cr.size(); i++) {
+		l.setText(l.getText()+cr.pop()+"");
+	}
+		
+		if(e.getKeyCode()==KeyEvent.VK_BACK_SPACE) {
+			
+			String lss =l.getText();
+			System.out.println(lss);
+			crd.push((char) (lss.length()-1));
+			l.setText(lss.substring(0, lss.length()-2));
+			
+		}
+		if(e.getKeyCode()==KeyEvent.VK_ENTER) {
+			for(int i= 0; i<crd.size(); i++) {
+			l.setText(l.getText()+crd.pop()+"");
+			}
+		}
+	}
+
+	
+	
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
